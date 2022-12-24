5f9e8e8bbe3fedf7d7cb4be9dbd8f908cc053f57
Moha  tawil <47723347+fwelpo3@users.noreply.github.com>
2022-12-24 03:23:49 +0100

    Add files via upload

--- Marrio Moma/Test.py
+++ Marrio Moma/Test.py
@@ -1,83 +1,3 @@
-# import pygame
-# import sys
-#
-# pygame.init()
-#
-# screen = pygame.display.set_mode((600,500))
-# screenSize = screen.get_width(),screen.get_height()
-# Clock = pygame.time.Clock()
-#
-# pygame.display.set_caption("Marrio")
-# plyer = pygame.image.load("unten.png").convert_alpha()
-# plyer_rect = plyer.get_rect()
-# #plyer_rect = plyer.get_rect()
-# #plyer1 = pygame.transform.scale(plyer, (50,50))
-#
-# plyer_image = [pygame.image.load("oben.png").convert_alpha(),
-#                pygame.image.load("unten.png").convert_alpha(),
-#                pygame.image.load("rechts.png").convert_alpha(),
-#                pygame.image.load("links.png").convert_alpha(),
-#                pygame.image.load("left1.png").convert_alpha(),
-#                pygame.image.load("right1.png").convert_alpha()]
-#
-# o_background =   pygame.image.load("background.jpg").convert_alpha()
-# background = pygame.transform.scale(o_background, screenSize)
-#
-# p_x, p_y = 100,200
-# p_g = 0
-# speed = 5
-# while True:
-#
-#
-#
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             sys.exit()
-#
-#
-#
-#     screen.blit(background,(0,0))
-#
-#
-#
-#
-#     if pygame.key.get_pressed()[pygame.K_UP]:
-#         plyer = plyer_image[0]
-#
-#         plyer_rect.y -= speed
-#     if pygame.key.get_pressed()[pygame.K_DOWN]:
-#         plyer = plyer_image[1]
-#         plyer_rect.y += speed
-#     if pygame.key.get_pressed()[pygame.K_RIGHT]:
-#         if pygame.time.get_ticks() % 100 < 50:
-#             plyer = plyer_image[2]
-#         else:
-#             plyer = plyer_image[5]
-#         plyer_rect.x += speed
-#     if pygame.key.get_pressed()[pygame.K_LEFT]:
-#         if pygame.time.get_ticks() % 100 < 50:
-#             plyer = plyer_image[3]
-#         else:
-#             plyer = plyer_image[4]
-#         plyer_rect.x -= speed
-#
-#
-#     if plyer_rect.bottom >= screen.get_height(): plyer_rect.bottom = screen.get_height()
-#     if plyer_rect.right >= screen.get_width(): plyer_rect.right = screen.get_width()
-#     if plyer_rect.top <= 0: plyer_rect.top = 0
-#     if plyer_rect.left <= 0: plyer_rect.left = 0
-#
-#
-#     screen.blit(plyer,plyer_rect)
-#
-#
-#     pygame.display.update()
-#     Clock.tick(60)
-
-
-
-
 import pygame
 import sys
 
@@ -94,6 +14,27 @@ plyer_image = [pygame.image.load("Bilder/oben.png").convert_alpha(),
                pygame.image.load("Bilder/links.png").convert_alpha(),
                pygame.image.load("Bilder/left1.png").convert_alpha(),
                pygame.image.load("Bilder/right1.png").convert_alpha()]
+
+def move_stone(x, y, plyer_rect):
+  keys = pygame.key.get_pressed()
+  if keys[pygame.K_UP]:
+    y -= speed
+  if keys[pygame.K_DOWN]:
+    y += speed
+  if keys[pygame.K_LEFT]:
+    x -= speed
+  if keys[pygame.K_RIGHT]:
+    x += speed
+
+    # Calculate the distance between the player and the stone
+  distance = plyer_rect.right - x
+
+  # If the player is on the left of the stone, the distance is 5 or lower, and the player is moving to the right, move the stone to the right as well
+  if plyer_rect.x < x and distance <= 5 and keys[pygame.K_d]:
+      x += speed
+Stein_img =        pygame.image.load("Bilder/Steine/1.png").convert_alpha()
+stein =        pygame.transform.smoothscale(Stein_img,(30,30))
+stein_rect = stein.get_rect(x=200, y=200)
 
 # Load the background image and scale it to fit the screen
 o_background = pygame.image.load("Bilder/background.jpg").convert_alpha()
@@ -136,6 +77,32 @@ while True:
             plyer = plyer_image[4]
         plyer_rect.move_ip(-speed, 0)
 
+    dist_rl1 = plyer_rect.top - stein_rect.top
+    dist_r2 = plyer_rect.right - stein_rect.left
+    dist_l2 = plyer_rect.left - stein_rect.right
+
+    dist_tb1 = plyer_rect.left - stein_rect.left
+    dist_t2 = plyer_rect.bottom - stein_rect.top
+    dist_b2 = plyer_rect.top - stein_rect.bottom
+
+
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    #Here the plyer gltiches, we have to play with the nums to fix it
+    if dist_rl1 >= -50 and dist_rl1 <= 20 and dist_r2 >-5 and dist_r2 <5 and keys[pygame.K_RIGHT]:
+        stein_rect.x += speed
+
+    if dist_rl1 >= -50 and dist_rl1 <= 20 and dist_l2 == 0  and keys[pygame.K_LEFT]:
+        stein_rect.x -= speed
+
+    if dist_tb1 >= -20 and dist_tb1 <= 20 and dist_t2 < 5 and dist_t2 >-4  and keys[pygame.K_DOWN]:
+        stein_rect.y += speed
+
+    if dist_tb1 >= -20 and dist_tb1 <= 20 and dist_b2 <= 0 and dist_b2 >-5  and keys[pygame.K_UP]:
+        stein_rect.y -= speed
+
+    if pygame.time.get_ticks()%1000 < 50/3:
+       print(dist_t2)
+
     # Keep the player within the screen boundaries
     if plyer_rect.bottom > screen_height:
         plyer_rect.bottom = screen_height
@@ -149,6 +116,8 @@ while True:
     # Clear the screen and draw the background and player
     screen.blit(background, (0, 0))
     screen.blit(plyer, plyer_rect)
+    screen.blit(stein, stein_rect)
+
 
     # Update the display
     pygame.display.update()
