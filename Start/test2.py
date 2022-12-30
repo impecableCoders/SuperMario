d44ae6a9ac2370fab362653df86c260fc43907dd
matmer2367 <62395278+matmer2367@users.noreply.github.com>
2022-12-30 22:03:12 +0100

    Update test2.py

--- Start/test2.py
+++ Start/test2.py
@@ -1,6 +1,12 @@
 import pygame
 import sys
 
+
+def exitGame():
+    pygame.quit()
+    sys.exit()
+
+
 # Initialize Pygame and create a window
 pygame.init()
 screen_width, screen_height = 600, 500
@@ -9,11 +15,8 @@ pygame.display.set_caption("Mario")
 clock = pygame.time.Clock()
 
 
-def image_plyer(image,defualt_width_sacle=30,defualt_height_sacle=50):
-
-
-
-    plyer_image = pygame.image.load('Bilder/'+image+'.png').convert_alpha()
+def load_image_plyer(image, defualt_width_sacle=30, defualt_height_sacle=50):
+    plyer_image = pygame.image.load('Bilder/' + image + '.png').convert_alpha()
     pygame.transform.scale(plyer_image, (defualt_width_sacle, defualt_height_sacle))
     return plyer_image
 
@@ -22,66 +25,96 @@ def image_stein():
     Stein_img = pygame.image.load("Bilder/Steine/1.png").convert_alpha()
     stein = pygame.transform.smoothscale(Stein_img, (30, 30))
     return stein
+
+
 def line(x_start, y_start, x_end, y_end):
     pygame.draw.line(screen, "black", (x_start, y_start), (x_end, y_end), width=5)
+
+
 def image_background():
     o_background1 = pygame.image.load("Bilder/background1.jpg").convert_alpha()
     background1 = pygame.transform.scale(o_background1, (screen_width, screen_height))
     return background1
 
 
-
-
 stein = image_stein()
 stein_rect = stein.get_rect(x=200, y=200)
 background = image_background()
 
-plyer = image_plyer("unten")
-plyer_rect = plyer.get_rect(x=100, y=200)
+playerAnimations = {"left1": load_image_plyer("left1"),
+                    "links": load_image_plyer("links"),
+                    "oben": load_image_plyer("oben"),
+                    "rechts": load_image_plyer("rechts"),
+                    "right1": load_image_plyer("right1"),
+                    "unten": load_image_plyer("unten")}
+
+
+def createPlayerDamagedAnimations():
+    keys = ["left1", "links", "oben", "rechts", "right1", "unten"]
+    res = {}
+    for k in keys:
+        img = load_image_plyer(k)
+        img.fill((255, 0, 0, 128), special_flags=pygame.BLEND_ADD)
+        res[k] = img
+    return res
+
+
+playerDamagedAnimations = createPlayerDamagedAnimations()
+playerAnimationCurrentKey = "unten"
+def activateDamageAnimationPlayer():
+    global lastDamageActiveTime
+    lastDamageActiveTime = pygame.time.get_ticks()
+def getPlayerSprite():
+    if damaged_animation:
+        return playerDamagedAnimations[playerAnimationCurrentKey]
+    return playerAnimations[playerAnimationCurrentKey]
 
 
 speed = 5
 line_x = 0
 hertz = 5
 
+damaged_animation = False
+lastDamageActiveTime = 0
+
+plyer = getPlayerSprite()
+plyer_rect = plyer.get_rect(x=100, y=200)
 
 # Main game loop
 while True:
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            exitGame()
 
     # Update the player's position based on key input
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]: exitGame()
 
-    
     if keys[pygame.K_RIGHT] and keys[pygame.K_LEFT]:
-        plyer = image_plyer("unten")
+        playerAnimationCurrentKey = "unten"
 
     if keys[pygame.K_UP]:
-        plyer = image_plyer("oben")
+        playerAnimationCurrentKey = "oben"
         plyer = pygame.transform.scale(plyer, (30, 50))
         plyer_rect.move_ip(0, -speed)
+
     if keys[pygame.K_DOWN]:
-        plyer = image_plyer("unten")
-        plyer = pygame.transform.scale(plyer, (30, 50))
+        playerAnimationCurrentKey = "unten"
         plyer_rect.move_ip(0, speed)
+
     if keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
         if pygame.time.get_ticks() % 100 < 50:
-            plyer = image_plyer("rechts")
-            plyer = pygame.transform.scale(plyer, (30, 50))
+            playerAnimationCurrentKey = "rechts"
         else:
-            plyer = image_plyer("right1")
-            plyer = pygame.transform.scale(plyer, (30, 50))
+            playerAnimationCurrentKey = "right1"
         plyer_rect.move_ip(speed, 0)
+
     if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
         if pygame.time.get_ticks() % 100 < 50:
-            plyer = image_plyer("links")
-            plyer = pygame.transform.scale(plyer, (30, 50))
+            playerAnimationCurrentKey = "links"
         else:
-            plyer = image_plyer("left1")
+            playerAnimationCurrentKey = "left1"
             plyer = pygame.transform.scale(plyer, (30, 50))
         plyer_rect.move_ip(-speed, 0)
 
@@ -91,31 +124,31 @@ while True:
     dist_right = plyer_rect.right - stein_rect.left
     dist_left = plyer_rect.left - stein_rect.right
 
-
-
     if plyer_rect.colliderect(stein_rect) and keys[pygame.K_RIGHT]:
         stein_rect.x += speed
         plyer_rect.x = plyer_rect.x - 50
+        activateDamageAnimationPlayer()
 
-        if plyer_rect.colliderect(stein_rect):
-            # Zeige Mario rot an
-            plyer = image_plyer("unten")
-            plyer = pygame.transform.scale(plyer, (30, 50))
-        else:
-
-            # Zeige Mario in seiner normalen Farbe an
-            plyer = image_plyer("rechtsrot")  # oder irgendein anderes Bild von Mario in der normalen Farbe
-            plyer = pygame.transform.scale(plyer, (30, 50))
+    #     # Zeige Mario rot an
+    #     plyer = playerDamagedAnimations["unten"]
+    #     plyer = pygame.transform.scale(plyer, (30, 50))
+    # else:
+    #     # Zeige Mario in seiner normalen Farbe an
+    #     plyer = getPlayerSprite["unten"]
+    #     plyer = pygame.transform.scale(plyer, (30, 50))
 
     if plyer_rect.colliderect(stein_rect) and keys[pygame.K_LEFT]:
         stein_rect.x -= speed
         plyer_rect.x = plyer_rect.x + 50
+        activateDamageAnimationPlayer()
     if plyer_rect.colliderect(stein_rect) and keys[pygame.K_DOWN]:
         stein_rect.y += speed
         plyer_rect.y = plyer_rect.y - 50
+        activateDamageAnimationPlayer()
     if plyer_rect.colliderect(stein_rect) and keys[pygame.K_UP]:
         stein_rect.y -= speed
         plyer_rect.y = plyer_rect.y + 50
+        activateDamageAnimationPlayer()
 
     # print to help
     if pygame.time.get_ticks() % 1000 < 50 / 3:
@@ -135,22 +168,30 @@ while True:
             stein_rect.y = plyer_rect.centery - 10
 
     # Keep the player within the screen boundaries
-    if plyer_rect.bottom > screen_height :
+    if plyer_rect.bottom > screen_height:
         plyer_rect.bottom = screen_height
-        stein_rect.y = plyer_rect.centery - 10
 
     if plyer_rect.right > screen_width:
         plyer_rect.right = screen_width
 
-    if plyer_rect.top < 0 :
-        plyer_rect.bottom = screen_height
-        stein_rect.y = plyer_rect.centery - 10
-
+    if plyer_rect.top < 0:
+        plyer_rect.top = 0
 
     if plyer_rect.top < 0:
         plyer_rect.top = 0
     if plyer_rect.left < 0:
         plyer_rect.left = 0
+
+    # Update Player Damage Animation
+    if pygame.time.get_ticks() - lastDamageActiveTime < 300:  # Damage flickering on
+        if pygame.time.get_ticks() % 100 < 50:
+            damaged_animation = True
+        else:
+            damaged_animation = False
+    else:
+        damaged_animation = False
+
+    plyer = getPlayerSprite()
 
     # Clear the screen and draw the background and player
     screen.blit(background, (0, 0))
