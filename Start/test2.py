6ed13b275a936cf52bd4c8226e1a4c7a5ac5ebe2
Moha  tawil <47723347+fwelpo3@users.noreply.github.com>
2022-12-31 19:25:52 +0100

    Stein Movement und kleine sachen
    1. Ich habe paar Sachen gelöscht, die unnötig waren.
    2. ich habe line auch gelöscht, falls wir die noch brauchen, dann können wir Test angucken.
    3. Ich habe neue Funk. gemacht, der einen Rect braucht um den auf das Fenster zu halten, Keep_The_Opj_On_Screen(plyer_rect).
    4. Neue Variable Shock_speed, wenn der Spieler, das Stein berührt dann sollte der geschoben werden.
    5. Stein Movement mit move_stone()

--- Start/test2.py
+++ Start/test2.py
@@ -1,11 +1,47 @@
-import pygame
-import sys
-
+import pygame, sys, random, time
 
 def exitGame():
     pygame.quit()
     sys.exit()
+def Keep_The_Opj_On_Screen(Opj_rect):
+    if Opj_rect.bottom > screen_height:
+        Opj_rect.bottom = screen_height
+    if Opj_rect.right > screen_width:
+        Opj_rect.right = screen_width
+    if Opj_rect.top < 0:
+        Opj_rect.top = 0
+    if Opj_rect.top < 0:
+        Opj_rect.top = 0
+    if Opj_rect.left < 0:
+        Opj_rect.left = 0
 
+def move_stone(stone, stone_rect, speed=2):
+
+    if random.random() < 0.1:
+        directions = ["S", "SW", "W", "NW", "N", "NE", "E", "SE"]
+        direction = random.choice(directions)
+
+
+        if direction == "S":
+            move_x, move_y = 0, speed
+        elif direction == "SW":
+            move_x, move_y = -speed, speed
+        elif direction == "W":
+            move_x, move_y = -speed, 0
+        elif direction == "NW":
+            move_x, move_y = -speed, -speed
+        elif direction == "N":
+            move_x, move_y = 0, -speed
+        elif direction == "NE":
+            move_x, move_y = speed, -speed
+        elif direction == "E":
+            move_x, move_y = speed, 0
+        elif direction == "SE":
+            move_x, move_y = speed, speed
+
+        # Add the relative coordinates to the stone's coordinates
+        stone_rect.left += move_x
+        stone_rect.top += move_y
 
 # Initialize Pygame and create a window
 pygame.init()
@@ -14,28 +50,18 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Mario")
 clock = pygame.time.Clock()
 
-
 def load_image_plyer(image, defualt_width_sacle=30, defualt_height_sacle=50):
     plyer_image = pygame.image.load('Bilder/' + image + '.png').convert_alpha()
     pygame.transform.scale(plyer_image, (defualt_width_sacle, defualt_height_sacle))
     return plyer_image
-
-
 def image_stein():
     Stein_img = pygame.image.load("Bilder/Steine/1.png").convert_alpha()
     stein = pygame.transform.smoothscale(Stein_img, (30, 30))
     return stein
-
-
-def line(x_start, y_start, x_end, y_end):
-    pygame.draw.line(screen, "black", (x_start, y_start), (x_end, y_end), width=5)
-
-
 def image_background():
     o_background1 = pygame.image.load("Bilder/background1.jpg").convert_alpha()
     background1 = pygame.transform.scale(o_background1, (screen_width, screen_height))
     return background1
-
 
 stein = image_stein()
 stein_rect = stein.get_rect(x=200, y=200)
@@ -48,7 +74,6 @@ playerAnimations = {"left1": load_image_plyer("left1"),
                     "right1": load_image_plyer("right1"),
                     "unten": load_image_plyer("unten")}
 
-
 def createPlayerDamagedAnimations():
     keys = ["left1", "links", "oben", "rechts", "right1", "unten"]
     res = {}
@@ -58,25 +83,24 @@ def createPlayerDamagedAnimations():
         res[k] = img
     return res
 
-
 playerDamagedAnimations = createPlayerDamagedAnimations()
 playerAnimationCurrentKey = "unten"
+
 def activateDamageAnimationPlayer():
     global lastDamageActiveTime
     lastDamageActiveTime = pygame.time.get_ticks()
+
 def getPlayerSprite():
     if damaged_animation:
         return playerDamagedAnimations[playerAnimationCurrentKey]
     return playerAnimations[playerAnimationCurrentKey]
 
-
-speed = 5
-line_x = 0
-hertz = 5
+Plyer_speed = 5
+Stein_speed = 5
+Shock_speed = 50
 
 damaged_animation = False
 lastDamageActiveTime = 0
-
 plyer = getPlayerSprite()
 plyer_rect = plyer.get_rect(x=100, y=200)
 
@@ -91,96 +115,52 @@ while True:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]: exitGame()
 
-    if keys[pygame.K_RIGHT] and keys[pygame.K_LEFT]:
-        playerAnimationCurrentKey = "unten"
-
+    # Plyer Movement
+    if keys[pygame.K_RIGHT] and keys[pygame.K_LEFT]:playerAnimationCurrentKey = "unten"
     if keys[pygame.K_UP]:
         playerAnimationCurrentKey = "oben"
-        plyer = pygame.transform.scale(plyer, (30, 50))
-        plyer_rect.move_ip(0, -speed)
-
+        plyer_rect.move_ip(0, -Plyer_speed)
     if keys[pygame.K_DOWN]:
         playerAnimationCurrentKey = "unten"
-        plyer_rect.move_ip(0, speed)
+        plyer_rect.move_ip(0, Plyer_speed)
 
+
+
+    # Plyer Animation
     if keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
         if pygame.time.get_ticks() % 100 < 50:
             playerAnimationCurrentKey = "rechts"
         else:
             playerAnimationCurrentKey = "right1"
-        plyer_rect.move_ip(speed, 0)
-
+        plyer_rect.move_ip(Plyer_speed, 0)
     if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
         if pygame.time.get_ticks() % 100 < 50:
             playerAnimationCurrentKey = "links"
         else:
             playerAnimationCurrentKey = "left1"
-            plyer = pygame.transform.scale(plyer, (30, 50))
-        plyer_rect.move_ip(-speed, 0)
+        plyer_rect.move_ip(-Plyer_speed, 0)
 
-    # Here is the movement of the stone
-    dist_bottom = plyer_rect.bottom - stein_rect.top
-    dist_top = plyer_rect.top - stein_rect.bottom
-    dist_right = plyer_rect.right - stein_rect.left
-    dist_left = plyer_rect.left - stein_rect.right
-
+    # The Plyer Shock
     if plyer_rect.colliderect(stein_rect) and keys[pygame.K_RIGHT]:
-        stein_rect.x += speed
-        plyer_rect.x = plyer_rect.x - 50
+        stein_rect.x += Stein_speed
+        plyer_rect.x = plyer_rect.x - Shock_speed
         activateDamageAnimationPlayer()
-
-    #     # Zeige Mario rot an
-    #     plyer = playerDamagedAnimations["unten"]
-    #     plyer = pygame.transform.scale(plyer, (30, 50))
-    # else:
-    #     # Zeige Mario in seiner normalen Farbe an
-    #     plyer = getPlayerSprite["unten"]
-    #     plyer = pygame.transform.scale(plyer, (30, 50))
-
     if plyer_rect.colliderect(stein_rect) and keys[pygame.K_LEFT]:
-        stein_rect.x -= speed
-        plyer_rect.x = plyer_rect.x + 50
+        stein_rect.x -= Stein_speed
+        plyer_rect.x = plyer_rect.x + Shock_speed
         activateDamageAnimationPlayer()
     if plyer_rect.colliderect(stein_rect) and keys[pygame.K_DOWN]:
-        stein_rect.y += speed
-        plyer_rect.y = plyer_rect.y - 50
+        stein_rect.y += Stein_speed
+        plyer_rect.y = plyer_rect.y - Shock_speed
         activateDamageAnimationPlayer()
     if plyer_rect.colliderect(stein_rect) and keys[pygame.K_UP]:
-        stein_rect.y -= speed
-        plyer_rect.y = plyer_rect.y + 50
+        stein_rect.y -= Stein_speed
+        plyer_rect.y = plyer_rect.y + Shock_speed
         activateDamageAnimationPlayer()
 
-    # print to help
-    if pygame.time.get_ticks() % 1000 < 50 / 3:
-        print("line: 118", "bottom:", dist_bottom)
-
-    # The line movement
-    if keys[pygame.K_e]:
-        if line_x < 50:
-            line_x += 1
-    if keys[pygame.K_q]:
-        if line_x > 0:
-            line_x -= 1
-
-    if stein_rect.x <= line_x + plyer_rect.left + 30 and 31 < dist_bottom < 56:
-        if keys[pygame.K_f]:
-            stein_rect.x = plyer_rect.x + line_x + 20
-            stein_rect.y = plyer_rect.centery - 10
-
-    # Keep the player within the screen boundaries
-    if plyer_rect.bottom > screen_height:
-        plyer_rect.bottom = screen_height
-
-    if plyer_rect.right > screen_width:
-        plyer_rect.right = screen_width
-
-    if plyer_rect.top < 0:
-        plyer_rect.top = 0
-
-    if plyer_rect.top < 0:
-        plyer_rect.top = 0
-    if plyer_rect.left < 0:
-        plyer_rect.left = 0
+    # Keep the plyer and the Stone on The Screen
+    Keep_The_Opj_On_Screen(plyer_rect)
+    Keep_The_Opj_On_Screen(stein_rect)
 
     # Update Player Damage Animation
     if pygame.time.get_ticks() - lastDamageActiveTime < 300:  # Damage flickering on
@@ -193,11 +173,13 @@ while True:
 
     plyer = getPlayerSprite()
 
+    # Move the stone in a random direction
+    move_stone(stein, stein_rect, 20)
+
     # Clear the screen and draw the background and player
     screen.blit(background, (0, 0))
     screen.blit(plyer, plyer_rect)
     screen.blit(stein, stein_rect)
-    line(plyer_rect.centerx + 15, plyer_rect.centery, plyer_rect.centerx + line_x + 15, plyer_rect.centery)
 
     # Update the display
     pygame.display.update()
